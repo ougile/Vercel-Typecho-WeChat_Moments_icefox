7b886914b5598816d350ca23c5a10e0af883003e
ougile <yxgzuzhi@gmail.com>
2024-09-11 20:55:48 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -13,6 +13,8 @@ define('__TYPECHO_SECURE__', true);
 /** 定义根目录 */
 define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
 
+define('__TYPECHO_DYNAMIC_SITE_URL__',true);
+
 /** 定义插件目录(相对路径) */
 define('__TYPECHO_PLUGIN_DIR__', '/usr/plugins');
 
